77a35132f8074c2f5d6bcd003ae105e94690501f
Theo Rubio <theo.rubio~hotmail.fr>
2025-04-24 16:05:05 +0200

    Fix login 500: defer supabase import, remove use client v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";  // or wherever your Tailwind/CSS lives
+import "../app/globals.css";  // or wherever your Tailwind/CSS lives
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
