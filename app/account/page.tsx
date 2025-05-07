4544e22b92e356326fccc01cc813f71209d8cf36
Theo Rubio <theo.rubio~hotmail.fr>
2025-05-07 16:33:53 +0200

    hotfixes searchparams import

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import {
   User as UserIcon,
   CreditCard,
